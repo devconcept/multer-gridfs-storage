0b38029a3d3855e50ce98714cdab7d67829d46f4
devconcept <devconcept@outlook.com>
2017-05-08 16:03:02 -0400

    Fixed code coverage bug with babel

--- test/utils/uploads.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const path = require('path');
-
-const files = ['sample1.jpg', 'sample2.jpg']
-  .map((file) => path.normalize(__dirname + '/../attachments/' + file));
-
-module.exports.files = files;
