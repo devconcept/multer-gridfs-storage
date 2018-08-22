e03d7d8fb12f3cf0b395433f326a2e5d72ad126a
devconcept <devconcept@outlook.com>
2018-08-22 09:48:30 -0500

    Fixed mocha install on older version of node

--- test/legacy.sh
+++ test/legacy.sh
@@ -2,6 +2,7 @@
 node_version=$(node -v);
 if  [ ${node_version:0:2} = "0." ]; then
   npm install mongodb@2;
+  npm install mocha@3;
   if  [ ${node_version:2:2} = 10 ]; then
     npm install es6-promise;
   fi
