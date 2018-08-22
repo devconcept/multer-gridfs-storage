60453e4e3226d5d70a210cb245af4d020e609104
devconcept <devconcept@outlook.com>
2018-08-22 10:56:54 -0500

    Updated dependency versions

--- test/legacy.sh
+++ test/legacy.sh
@@ -5,8 +5,6 @@ echo "Running in node " ${node_version};
 if  [ ${node_version:1:2} = "0." ]; then
   echo "Installing legacy dependencies";
   npm install mongodb@2;
-  npm install mocha@3;
-  npm install chai@3;
   if  [ ${node_version:3:2} = 10 ]; then
     echo "Installing promise support";
     npm install es6-promise;
