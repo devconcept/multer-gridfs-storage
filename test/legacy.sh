e2cfd0801adb523490c4a4bae8dd830578460145
devconcept <devconcept@outlook.com>
2018-08-22 11:14:04 -0500

    Fixed supertest install

--- test/legacy.sh
+++ test/legacy.sh
@@ -9,7 +9,7 @@ if  [ ${node_version:1:2} = "0." ]; then
   npm install mocha@3;
   npm install chai@3;
   npm install sinon-chai@2;
-  npm install superstest@1;
+  npm install supertest@1;
   if  [ ${node_version:3:2} = 10 ]; then
     echo "Installing promise support";
     npm install es6-promise;
