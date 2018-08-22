aa5e79d9ec418a9b938803f4c9c3b428e0ec8882
devconcept <devconcept@outlook.com>
2018-08-22 11:09:00 -0500

    Revert "Updated dependency versions"
    This reverts commit 60453e4e3226d5d70a210cb245af4d020e609104.

--- test/legacy.sh
+++ test/legacy.sh
@@ -4,7 +4,12 @@ node_version=$(node -v);
 echo "Running in node " ${node_version};
 if  [ ${node_version:1:2} = "0." ]; then
   echo "Installing legacy dependencies";
+  npm uninstall sinon-chai;
   npm install mongodb@2;
+  npm install mocha@3;
+  npm install chai@3;
+  npm install sinon-chai@2;
+  npm install superstest@1;
   if  [ ${node_version:3:2} = 10 ]; then
     echo "Installing promise support";
     npm install es6-promise;
