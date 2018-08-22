be0bd91048ded3510a96d26e5df684bed521baa7
devconcept <devconcept@outlook.com>
2018-08-22 10:10:03 -0500

    Downgrade chai in the install list

--- test/legacy.sh
+++ test/legacy.sh
@@ -1,8 +1,10 @@
-#! /bin/bash
+#!/bin/bash
+set -ev
 node_version=$(node -v);
 if  [ ${node_version:0:2} = "0." ]; then
   npm install mongodb@2;
   npm install mocha@3;
+  npm install chai@3;
   if  [ ${node_version:2:2} = 10 ]; then
     npm install es6-promise;
   fi
