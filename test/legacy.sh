99a7a5397a6d1e671166ffb9d73d0dd86825693e
devconcept <devconcept@outlook.com>
2018-08-22 10:49:30 -0500

    Fixed correct node version slicing

--- test/legacy.sh
+++ test/legacy.sh
@@ -2,12 +2,12 @@
 set -ev
 node_version=$(node -v);
 echo "Running in node " ${node_version};
-if  [ ${node_version:0:2} = "0." ]; then
+if  [ ${node_version:1:2} = "0." ]; then
   echo "Installing legacy dependencies";
   npm install mongodb@2;
   npm install mocha@3;
   npm install chai@3;
-  if  [ ${node_version:2:2} = 10 ]; then
+  if  [ ${node_version:3:2} = 10 ]; then
     echo "Installing promise support";
     npm install es6-promise;
   fi
