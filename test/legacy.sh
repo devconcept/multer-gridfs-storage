c91357de70d63ebfd665f344be9e27545d82bc3d
devconcept <devconcept@outlook.com>
2018-08-22 11:25:01 -0500

    Added sinon to legacy dependencies

--- test/legacy.sh
+++ test/legacy.sh
@@ -8,6 +8,7 @@ if  [ ${node_version:1:2} = "0." ]; then
   npm install mongodb@2;
   npm install mocha@3;
   npm install chai@3;
+  npm install sinon@2;
   npm install sinon-chai@2;
   npm install supertest@1;
   if  [ ${node_version:3:2} = 10 ]; then
