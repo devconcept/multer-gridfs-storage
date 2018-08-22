5bc756621dd119920e65d8397707e02ce9766eb1
devconcept <devconcept@outlook.com>
2018-08-22 10:37:15 -0500

    Added logs in build step

--- test/legacy.sh
+++ test/legacy.sh
@@ -2,10 +2,12 @@
 set -ev
 node_version=$(node -v);
 if  [ ${node_version:0:2} = "0." ]; then
+  echo "Installing legacy dependencies";
   npm install mongodb@2;
   npm install mocha@3;
   npm install chai@3;
   if  [ ${node_version:2:2} = 10 ]; then
+    echo "Installing promise support";
     npm install es6-promise;
   fi
 fi
