979d717e725b015aa1c48174654f2716526e9d70
devconcept <devconcept@outlook.com>
2019-10-15 09:42:25 -0400

    Added generators example

---
+++ examples/generators/routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+
+const {url} = require('./settings');
+/* eslint-disable-next-line new-cap */
+const router = express.Router();
+const {generateBytes} = GridFsStorage;
+
+const genStorage = new GridFsStorage({
+	url,
+	async *file(req, file) {
+		let counter = 0;
+		for (;;) {
+			/* eslint-disable-next-line no-await-in-loop */
+			const {filename} = await generateBytes();
+			[, file] = yield `${file.originalname}_${filename}_${counter}`;
+			counter++;
+		}
+	}
+});
+const genUpload = multer({storage: genStorage});
+router.post('/gen', genUpload.single('field'), (req, res) => {
+	res.status(201).send('File uploaded');
+});
+
+module.exports = router;
